5f5787be3508427bc2d94bcf629ee6e7785d10ab
Jax I <jaxpi85@gmail.com>
2023-09-18 14:06:15 -0400

    added items.js to be used for elixers, weapons, and outfits for the player

---
+++ data/items.js
@@ -0,0 +1,56 @@
+// need to make a class for these
+
+// benefits of elixers lasts only for the battle being used in, weapon benefits lasts as long as equipped
+
+const items = {
+    HealthPotion: {
+      name: "Health Potion",
+      benefit: "Health + 25%",
+      type: "elixer"
+    },
+    PowerPotion: {
+        name: "Power Potion",
+        benefit: "Damage + 25%",
+        type: "elixer"
+    },
+    ShieldPotion: {
+        name: "Shield Potion",
+        benefit: "Defense + 25%",
+        type: "elixer"
+    },
+    Staff: {
+        name: "Staff",
+        benefit: "Health, Damage, and Defense + 5%",
+        type: "weapon"
+    },
+    Sword: {
+        name: "Sword",
+        benefit: "Damage + 10%, Defense + 5%",
+        type: "weapon"
+    },
+    Axe: {
+        name: "Axe",
+        benefit: "Defense + 10%, Damage + 5%",
+        type: "weapon"
+    },
+    Nomad: {
+        name: "Nomad",
+        benefit: "Health, Damage, and Defense + 5%",
+        type: "outfit"
+    },
+    Warrior: {
+        name: "Warrior",
+        benefit: "Damage + 10%, Defense + 5%",
+        type: "outfit"
+    },
+    Guardian: {
+        name: "Guardian",
+        benefit: "Defense + 10%, Damage + 5%",
+        type: "outfit"
+    },
+    Mystic: {
+        name: "Mystic",
+        benefit: "Health + 10%, Damage + 2.5%, Defense + 2.5%",
+        type: "outfit"
+    },
+  };
